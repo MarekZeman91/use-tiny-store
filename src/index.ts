77507831b90a25e0d73c4c617ea02bd62a6b228a
Marek Zeman <marekzeman91@gmail.com>
2025-03-10 17:29:58 +0100

    feat: Add detailed hook documentation

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,15 @@ const useStateWrapper = <S, A>(stateBuilder: () => ObservableState<S> & A, noEmi
   return state;
 };
 
+/**
+ * A hook to create a store with state and custom actions, allowing reactive state management.
+ *
+ * @param store - The initial state of the store or a factory function to produce the initial state.
+ * @param actions - A function that defines actions bound to the provided state.
+ * @param [noEmitChanges=false] - A flag indicating whether changes to the state should be emitted.
+ *
+ * @returns A combined object containing the observable state and bound actions.
+ */
 export function useStore<
   S extends Record<string, any>,
   A extends Record<string, Function>,
@@ -25,10 +34,26 @@ export function useStore<
   return useStateWrapper(() => createStore(store, actions), noEmitChanges);
 }
 
+/**
+ * A custom hook that creates and manages an observable state.
+ *
+ * @param initialValue - The initial state or a factory function to produce the initial state.
+ * @param [noEmitChanges=false] - A flag indicating whether changes to the state should be emitted.
+ * @returns The observable state.
+ */
 export function useObservable<T>(initialValue: T | (() => T), noEmitChanges = false): ObservableState<T> {
   return useStateWrapper(() => createObservable(initialValue), noEmitChanges);
 }
 
+/**
+ * Hook to subscribe to an observable state and retrieve its value.
+ * This allows component to reactively listen to changes in the observable state.
+ *
+ * @param observable - The observable state object to subscribe to.
+ * @param [getter] - Optional function to derive a value from the state.
+ * Defaults to a function that returns the current state as is.
+ * @returns The current derived value of the observable state.
+ */
 export function useValue<S, V = S>(
   observable: ObservableState<S>,
   getter: ((state: S) => V) = ((state) => state as unknown as V),
